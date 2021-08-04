dac392331d4e1914567a35914b7ab52851859b30
kombat <avkombatov@yandex.ru>
2021-08-04 22:51:18 +0300

    доработано меню при ширине экрана менее 768px, доработан слайдер блока brands

--- script.js
+++ script.js
@@ -20,38 +20,47 @@ for (item of doubtsTop) {
 let leftShift = document.querySelector('.slider-prev');
 let rightShift = document.querySelector('.slider-next');
 let left = 0;
-// let right = 0;
-
-leftShift.addEventListener('click', function () {
-    let brandSlider = document.querySelector('.brands__slider');
-    left = left - 94;
-    if (left < -564) {
-        left = 0;
-    }
-    brandSlider.style.left = left + 'px';
-})
+let right = 0;
 
 rightShift.addEventListener('click', function () {
     let brandSlider = document.querySelector('.brands__slider');
-    left = left + 94;
-    if (left > 0) {
-        left = 0;
+    right = right - 94;
+    if (right < -564) {
+        right = 0;
     }
-    brandSlider.style.left = left + 'px';
+    brandSlider.style.right = right + 'px';
 })
 
-//выпадающее меню на маленьком экране
+leftShift.addEventListener('click', function () {
+    let brandSlider = document.querySelector('.brands__slider');
+    right = right + 94;
+    if (right > 0) {
+        right = 0;
+    }
+    brandSlider.style.right = right + 'px';
+})
+
+
+
+//выпадающее меню на экране менее 768px
+
 let headerMenu = document.querySelector('.header__menu');
 let mobileImg = document.querySelector('.mobile__img');
 let menuClose = document.querySelector('.menu__close');
 
-mobileImg.addEventListener('click', function () {
+mobileImg.addEventListener('click', function () { //открытие меню
     headerMenu.style.display = "block";
 });
-
-menuClose.addEventListener('click', function () {
+menuClose.addEventListener('click', function () { // закрытие меню
     headerMenu.style.display = "none";
 });
 
-
-
+window.addEventListener("resize", function () { // отслеживание размера экрана
+    if (innerWidth <= 768) {
+        mobileImg.addEventListener('click', function () {
+            headerMenu.style.display = "block";
+        });
+    } else {
+        headerMenu.style.display = "none";
+    }
+});
